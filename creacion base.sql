9b5e4956af0779c2ab0b8cc10dbd11fc9dec031a
Fracontre <fco.contreras.lopez@gmail.com>
2024-06-17 21:48:57 -0400

    Update creacion base.sql
    poliza sometida a propuesta

--- creacion base.sql
+++ creacion base.sql
@@ -1,9 +1,9 @@
 CREATE TABLE Cliente (
-    cl_run_cliente VARCHAR(20) PRIMARY KEY,
-    cl_nombre VARCHAR(100),
-    cl_fecha_nacimiento DATE,
-    cl_direccion VARCHAR(255),
-    cl_telefono VARCHAR(20)
+    cl_RUN_Cliente VARCHAR(20) PRIMARY KEY,
+    cl_Nombre VARCHAR(100),
+    cl_Fecha_Nacimiento DATE,
+    cl_Direccion VARCHAR(255),
+    cl_Telefono VARCHAR(20)
 );
 -- esto es una prueba 
 CREATE TABLE Ejecutivo_de_venta (
@@ -41,47 +41,46 @@ CREATE TABLE Corporativo (
 );
 
 CREATE TABLE Cotizacion (
-    coti_Numero_Cotizacion INT PRIMARY KEY,
-    coti_Canal VARCHAR(50),
-    coti_Prima DECIMAL(10, 2),
-    coti_Fecha_Cotizacion DATE,
-    coti_ID_Ejecutivo INT, -- esto no debería existir
-    coti_RUN_Cliente VARCHAR(20), -- esto no debería existir
-    ej_ID_Ejecutivo,
-    cl_RUN_Cliente)
+    Numero_Cotizacion INT PRIMARY KEY,
+    Canal VARCHAR(50),
+    Prima DECIMAL(10, 2),
+    Fecha_Cotizacion DATE,
+    ID_Ejecutivo INT,
+    RUN_Cliente VARCHAR(20),
+    FOREIGN KEY (ID_Ejecutivo) REFERENCES Ejecutivo_de_venta(ID_Ejecutivo),
+    FOREIGN KEY (RUN_Cliente) REFERENCES Cliente(RUN_Cliente)
 );
 
-CREATE TABLE Poliza_sometida (
-    pol_som_Numero_Propuesta INT PRIMARY KEY,
-    pol_som_Prima DECIMAL(10, 2),
-    pol_som_Estado VARCHAR(50),
-    pol_som_Frecuencia_Pago VARCHAR(50),
-    pol_som_Fecha_Ingreso DATE,
-    pol_som_ID_Operacion INT,
-    FOREIGN KEY (ID_Operacion) REFERENCES Operacion(ID_operacion)
+CREATE TABLE propuesta (
+    pro_numero_propuesta INT PRIMARY KEY,
+    pro_prima DECIMAL(10, 2),
+    pro_estado VARCHAR(50),
+    pro_frecuencia_pago VARCHAR(50),
+    pro_fecha_ingreso DATE,
+    pro_op_id_operacion INT,
 );
 
 CREATE TABLE Poliza_emitida (
-    pol_emi_Numero_Poliza INT PRIMARY KEY,
-    pol_emi_Prima DECIMAL(10, 2),
-    pol_emi_Fecha_Emision DATE,
-    pol_emi_Frecuencia_Pago VARCHAR(50),
-    pol_emi_Estado_P_Emitida VARCHAR(50),
-    pol_emi_ID_Operacion INT,
+    Numero_Poliza INT PRIMARY KEY,
+    Prima DECIMAL(10, 2),
+    Fecha_Emision DATE,
+    Frecuencia_Pago VARCHAR(50),
+    Estado_P_Emitida VARCHAR(50),
+    ID_Operacion INT,
     FOREIGN KEY (ID_Operacion) REFERENCES Operacion(ID_operacion)
 );
 
 CREATE TABLE Operacion (
-    ops_ID_operacion INT PRIMARY KEY,
-    ops_Fecha_Aprobacion DATE,
-    ops_Responsable VARCHAR(100)
+    ID_operacion INT PRIMARY KEY,
+    Fecha_Aprobacion DATE,
+    Responsable VARCHAR(100)
 );
 
 CREATE TABLE Producto (
-    prod_Nombre_Producto VARCHAR(100) PRIMARY KEY,
-    prod_Tipo_Producto VARCHAR(50),
-    prod_Beneficio VARCHAR(255),
-    prod_Cobertura VARCHAR(255)
+    Nombre_Producto VARCHAR(100) PRIMARY KEY,
+    Tipo_Producto VARCHAR(50),
+    Beneficio VARCHAR(255),
+    Cobertura VARCHAR(255)
 );
 
 CREATE TABLE Cotizacion_producto_sometida_emitida (
